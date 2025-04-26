8d272e29b0747aa9af004f5416c969e23859fa7e
Zoneida <zoneidavente123@gmail.com>
2025-04-26 16:48:28 -0500

    Agregar botón de eliminar tareas

--- tareas_pro/src/App.vue
+++ tareas_pro/src/App.vue
@@ -1,14 +1,18 @@
 <script setup>
 import TaskForm from './components/TaskForm.vue'
+import MyTasks from './components/MyTasks.vue'  
+import { useTask } from '@/composables/useTask'  
+
+const { addItem } = useTask()  
 </script>
 
 <template>
   <TaskForm @add-task="addItem" />
+  <MyTasks />
 </template>
 
 <style scoped>
-
-body{
+body {
   padding: 0;
   margin: 0;
 }
